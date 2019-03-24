4afbbb29b538d8877c067afca261a4e608f4cb49
f4exb <f4exb06@gmail.com>
2019-03-24 21:44:05 +0100

    WSJT-X: check number of cores for compilation

--- wsjtx/build.sh
+++ wsjtx/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+nb_cores=$(grep -c ^processor /proc/cpuinfo)
 IMAGE_NAME=wsjtx/bionic:libfaketime
-DOCKER_BUILDKIT=1 docker build -t ${IMAGE_NAME} .
+DOCKER_BUILDKIT=1 docker build \
+    --build-arg nb_cores=${nb_cores} \
+    -t ${IMAGE_NAME} .
