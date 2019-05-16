ebb4bb804e5f4771293a4b512de5e969009e7088
f4exb <f4exb06@gmail.com>
2019-05-16 23:50:46 +0200

    SDRangel GUI: SoapyUHD support: updated starting script to add UHD dependency

--- sdrangel/start_gui.sh
+++ sdrangel/start_gui.sh
@@ -3,5 +3,5 @@ sudo service ssh start
 sudo service dbus start
 sudo service avahi-daemon start
 IPADDR=$(ip addr show type veth | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | head -1)
-export LD_LIBRARY_PATH=/opt/install/libsdrplay/lib:/opt/install/xtrx-images/lib
+export LD_LIBRARY_PATH=/opt/install/libsdrplay/lib:/opt/install/xtrx-images/lib:/opt/install/uhd/lib
 /bin/bash --init-file <(echo "/opt/install/sdrangel/bin/sdrangel -geometry 600x400+50+50 -a ${IPADDR}") # Drop to a shell when exiting SDRangel
