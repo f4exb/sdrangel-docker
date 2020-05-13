cd348c0a0664f936366aa502f18f9e8f2448e3f2
f4exb <f4exb06@gmail.com>
2020-05-13 16:16:04 +0200

    Added SDRangelSpectrum to compose stack

--- compose/run.sh
+++ compose/run.sh
@@ -5,7 +5,7 @@ OPTIND=1         # Reset in case getopts has been used previously in the shell.
 # Get options:
 show_help() {
   cat << EOF
-  Usage: ${0##*/} [-g] [-f flavor] [-t tag] [-T tag] [-n name] [-w port] [-s port] [-a port] [-u port[-port]] [-h]
+  Usage: ${0##*/} [-g] [-f flavor] [-t tag] [-T tag] [-n name] [-w port] [-s port] [-a port] [-u port[-port]] [-p port[-port]] [-h]
   Run SDRangel and SDRangelCli in a Docker compose stack.
   -D         use this option to bring down the compose stack (default is to bring up).
              Use the same -g and -c options if any that you used to bring up the stack.
@@ -14,12 +14,14 @@ show_help() {
   -f flavor  Image flavor. Can be vanilla, nvidia, server16, server24 (default vanilla). Use a flavor relevant to GUI or server variants.
   -t tag     Docker SDRangel image tag (default latest)
   -T tag     Docker SDRangelCli image tag (default latest)
+  -S tag     Docker SDRangelSpectrum image tag (default latest)
   -c name    Docker compose stack name (default compose)
   -n         Container name suffix (default 1)
   -w port    Web client port map to 8080 (default 8080)
-  -s port    SSH port map to 22 (default 50022).
+  -s port    Spectrum port map to 8081 (default 8081).
   -a port    API port map to 8091 (default 8091).
   -u port(s) UDP port(s) map to same with UDP option (default 9090). You can specify a range as XXXX-YYYY.
+  -p port(s) TCP port(s) map to same with TCP option (default 8887). You can specify a range as XXXX-YYYY.
   -h         Print this help.
   Examples:
     ./run.sh -g (starts sdrangel/vanilla:latest and sdrangelcli:latest)
@@ -32,16 +34,18 @@ EOF
 sdrangel_flavor="vanilla"
 image_tag="latest"
 image_tag_cli="latest"
+image_tag_spec="latest"
 name_suffix="1"
 stack_name=""
 web_port="8080"
-ssh_port="50022"
+spec_port="8081"
 api_port="8091"
 udp_port="9090"
+tcp_port="8887"
 run_gui=0
 action="up -d"
 
-while getopts "h?Dgf:t:T:c:w:s:a:u:" opt; do
+while getopts "h?Dgf:t:T:c:w:s:a:u:p:" opt; do
     case "$opt" in
     h|\?)
         show_help
@@ -57,17 +61,21 @@ while getopts "h?Dgf:t:T:c:w:s:a:u:" opt; do
         ;;
     T)  image_tag_cli=${OPTARG}
         ;;
+    S)  image_tag_spec=${OPTARG}
+        ;;
     c)  stack_name="-p ${OPTARG}"
         ;;
     n)  name_suffix=${OPTARG}
         ;;
     w)  web_port=${OPTARG}
         ;;
-    s)  ssh_port=${OPTARG}
+    s)  spec_port=${OPTARG}
         ;;
     a)  api_port=${OPTARG}
         ;;
     u)  udp_port=${OPTARG}
+        ;;
+    p)  tcp_port=${OPTARG}
         ;;
     esac
 done
@@ -86,11 +94,13 @@ export USER_UID=$(id -u)
 export SDRANGEL_FLAVOR=${sdrangel_flavor}
 export IMAGE_VERSION=${image_tag}
 export IMAGE_VERSION_CLI=${image_tag_cli}
+export IMAGE_VERSION_SPEC=${image_tag_spec}
 export NAME_SUFFIX=${name_suffix}
 export WEB_PORT=${web_port}
-export SSH_PORT=${ssh_port}
+export SPEC_PORT=${spec_port}
 export API_PORT=${api_port}
 export UDP_PORT=${udp_port}
+export TCP_PORT=${tcp_port}
 
 if [ "$run_gui" -eq 1 ]; then
     docker-compose -f compose_gui.yml ${stack_name} ${action}
